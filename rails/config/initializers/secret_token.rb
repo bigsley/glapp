966a482b8917b2fcc495539466a0e6bcdaf1184a
bigsley <bigsley@gmail.com>
2014-10-30 23:13:51 -0700

    Initial commit

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glapp::Application.config.secret_token = 'cc8c8fefa0e4be012982ce90a5f665da97c62cecc4bf4bb4d329608e44ee16f68ee1149b88ba84d0323b3cc9a3d82673dbcfe5c6ffb896cb13a68c9d5db06c48'
